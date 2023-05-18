b3e4247b156997c90906098fd92e544557cfa09f
yukkit.zhang <yukkit.zhang@cnosdb.com>
2023-05-18 12:19:07 +0800

    improve: inline_table_scan&subquery_alias

--- datafusion/optimizer/src/push_down_projection.rs
+++ datafusion/optimizer/src/push_down_projection.rs
@@ -227,7 +227,9 @@ impl OptimizerRule for PushDownProjection {
                     .iter()
                     .map(|c| {
                         replace_map.get(c).cloned().ok_or_else(|| {
-                            DataFusionError::Internal("replace column failed".to_string())
+                            DataFusionError::Internal(format!(
+                                "replace column {c} failed, valid: {replace_map:?}"
+                            ))
                         })
                     })
                     .collect::<Result<HashSet<_>>>()?;
